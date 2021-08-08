36e53f4a4a8942b4aa63a2de24f297af5a5176dd
elmedben <elmehdi.benammi@gmail.com>
2021-08-08 08:43:43 +0000

    Adding starting verb function

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,4 +1,3 @@
-
 import sqlite3
 import pandas as pd
 import re
@@ -7,7 +6,7 @@ from joblib import dump
 import nltk
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
+from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.multioutput import MultiOutputClassifier
 from sklearn.metrics import classification_report
@@ -16,117 +15,175 @@ from sklearn.model_selection import GridSearchCV
 nltk.download(["punkt", "wordnet", "averaged_perceptron_tagger"])
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
+from sklearn.base import BaseEstimator, TransformerMixin
 
+# ------------------------------------------------------------------------------
 
-#------------------------------------------------------------------------------
 
 def load_data(database_filepath):
-    
-    #create engine connection to database
+
+    # create engine connection to database
     con = sqlite3.connect("{}".format(database_filepath))
     cur = con.cursor()
-    
-    #read the sql db with a pandas query
+
+    # read the sql db with a pandas query
     df = pd.read_sql("SELECT * FROM messages", con)
-    
-    #seperate the needed data and drop unused columns
+
+    # seperate the needed data and drop unused columns
     X = df["message"].tolist()
     y = df.drop(columns=["message", "index"])
     categories_names = y.columns
-    
-    #close connection to engine
+
+    # close connection to engine
     con.close()
-    
+
     return X, y, categories_names
 
-#------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+
 
 def tokenize(text):
-    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    url_regex = (
+        "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+    )
 
-    detected_urls=re.findall(url_regex,text)
+    detected_urls = re.findall(url_regex, text)
 
     for url in detected_urls:
-        text=text.replace(url, 'urlplaceholder')
+        text = text.replace(url, "urlplaceholder")
 
-    tokens=word_tokenize(text)
+    tokens = word_tokenize(text)
 
-    lemmatizer=WordNetLemmatizer()
+    lemmatizer = WordNetLemmatizer()
 
-    clean_tokens=[]
+    clean_tokens = []
 
-    for tok in tokens : 
-        clean_tok=lemmatizer.lemmatize(tok).lower().strip()
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
         clean_tokens.append(clean_tok)
-    
-    return clean_tokens    
 
-#------------------------------------------------------------------------------
+    return clean_tokens
+
+
+# ------------------------------------------------------------------------------
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+    """
+    Starting Verb Extractor class
+
+    This class extract the starting verb of a sentence,
+    creating a new feature for the ML classifier
+    """
+
+    def starting_verb(self, text):
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            first_word, first_tag = pos_tags[0]
+            if first_tag in ["VB", "VBP"] or first_word == "RT":
+                return True
+        return False
+
+    # Given it is a tranformer we can return the self
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
+
+
+# ------------------------------------------------------------------------------
+
 
 def build_model():
 
     pipeline = Pipeline(
         [
-            ("vect", CountVectorizer(tokenizer=tokenize)),
-            ("tfidf", TfidfTransformer()),
-            ("multi_out_clf", MultiOutputClassifier(AdaBoostClassifier())),
+            (
+                "features",
+                FeatureUnion(
+                    [
+                        (
+                            "text_pipeline",
+                            Pipeline(
+                                [
+                                    (
+                                        "count_vectorizer",
+                                        CountVectorizer(tokenizer=tokenize),
+                                    ),
+                                    ("tfidf_transformer", TfidfTransformer()),
+                                ]
+                            ),
+                        ),
+                        ("starting_verb_transformer", StartingVerbExtractor()),
+                    ]
+                ),
+            ),
+            ("classifier", MultiOutputClassifier(AdaBoostClassifier())),
         ]
     )
-    
-
 
     return pipeline
 
-#------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+
 
 def evaluate_model(model, X_test, y_test):
-    
-    
+
     y_pred = model.predict(X_test)
 
     for idv, label in enumerate(y_test.columns):
         print(classification_report(y_test[label], y_pred[:, idv]))
-        
-#------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
+
 
 def save_model(model, model_filepath):
     dump(model, model_filepath)
 
-#------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+
 
 def main():
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
-        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        print("Loading data...\n    DATABASE: {}".format(database_filepath))
         X, y, category_names = load_data(database_filepath)
         y = y.drop(columns="genre")
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-        
-        print('Building model...')
+
+        print("Building model...")
         model = build_model()
-        
-        print('Training model...')
+
+        print("Training model...")
         model.fit(X_train, y_train)
-        
-        print('Evaluating model...')
+
+        print("Evaluating model...")
         evaluate_model(model, X_test, y_test)
 
-        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        print("Saving model...\n    MODEL: {}".format(model_filepath))
         save_model(model, model_filepath)
 
-        print('Trained model saved!\n')
-        
-        print('overall testing score is : ', model.score(X_test,y_test),'\n')
-        
-        print('overall training score is : ', model.score(X_train,y_train))
+        print("Trained model saved!\n")
+
+        print("overall testing score is : ", model.score(X_test, y_test), "\n")
+
+        print("overall training score is : ", model.score(X_train, y_train))
 
     else:
-        print('Please provide the filepath of the disaster messages database '\
-              'as the first argument and the filepath of the pickle file to '\
-              'save the model to as the second argument. \n\nExample: python '\
-              'models/train_classifier.py data/DisasterResponse.db models/classifier.pkl')
+        print(
+            "Please provide the filepath of the disaster messages database "
+            "as the first argument and the filepath of the pickle file to "
+            "save the model to as the second argument. \n\nExample: python "
+            "models/train_classifier.py data/DisasterResponse.db models/classifier.pkl"
+        )
 
-#------------------------------------------------------------------------------
 
-if __name__ == '__main__':
+# ------------------------------------------------------------------------------
+
+if __name__ == "__main__":
     main()
