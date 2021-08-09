8931db01c4d70649c3aa47b765c6e2b8cd2a7d8a
elmedben <elmehdi.benammi@gmail.com>
2021-08-09 14:35:38 +0000

    Modification of functions  readme and testing of function

--- app/run.py
+++ app/run.py
@@ -1,3 +1,4 @@
+# importing necessary libraries
 import json
 import plotly
 import pandas as pd
@@ -11,10 +12,20 @@ from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
 from sqlalchemy import create_engine
 
-
+# instantiating the flask app
 app = Flask(__name__)
 
+# simple tokenizing function for input text
 def tokenize(text):
+    """
+    Function that apply simple preprocessing for nlp by removing url, tokenizing and lematizing. It outputs clean tokens
+
+    args:
+        text : text element from the base list that will be preprocessed
+
+    returns:
+        clean_tokens: list of clean tokens
+    """
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
 
@@ -25,99 +36,77 @@ def tokenize(text):
 
     return clean_tokens
 
+
 # load data
-engine = create_engine('sqlite:///data/DisasterResponse.db')
-df = pd.read_sql_table('messages', engine)
+engine = create_engine("sqlite:///data/DisasterResponse.db")
+df = pd.read_sql_table("messages", engine)
 
 # load model
 model = load("models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
-@app.route('/')
-@app.route('/index')
+@app.route("/")
+@app.route("/index")
 def index():
-    
+
     # extract data needed for visuals
     # genre counts for plotting
-    genre_counts = df.groupby('genre').count()['message']
+    genre_counts = df.groupby("genre").count()["message"]
     genre_names = list(genre_counts.index)
-    
-    #related stat count for ploting
-    related_counts = df.groupby('related').count()['message']
-    related_names = ['not related', 'related']
- 
-    
+
+    # related stat count for ploting
+    related_counts = df.groupby("related").count()["message"]
+    related_names = ["not related", "related"]
+
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
         {
-            'data': [
-                Bar(
-                    x=genre_names,
-                    y=genre_counts
-                )
-            ],
-
-            'layout': {
-                'title': 'Distribution of Message Genres',
-                'yaxis': {
-                    'title': "Count"
-                },
-                'xaxis': {
-                    'title': "Genre"
-                }
-            }
+            "data": [Bar(x=genre_names, y=genre_counts)],
+            "layout": {
+                "title": "Distribution of Message Genres",
+                "yaxis": {"title": "Count"},
+                "xaxis": {"title": "Genre"},
+            },
         },
         {
-            'data': [
-                Bar(
-                    x=related_names,
-                    y=related_counts
-                )
-            ],
-
-            'layout': {
-                'title': 'Number of messages classified as related or not',
-                'yaxis': {
-                    'title': "Count"
-                },
-                'xaxis': {
-                    'title': "Genre"
-                }
-            }
-        }
+            "data": [Bar(x=related_names, y=related_counts)],
+            "layout": {
+                "title": "Number of messages classified as related or not",
+                "yaxis": {"title": "Count"},
+                "xaxis": {"title": "Genre"},
+            },
+        },
     ]
-    
+
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-    
+
     # render web page with plotly graphs
-    return render_template('master.html', ids=ids, graphJSON=graphJSON)
+    return render_template("master.html", ids=ids, graphJSON=graphJSON)
 
 
 # web page that handles user query and displays model results
-@app.route('/go')
+@app.route("/go")
 def go():
     # save user input in query
-    query = request.args.get('query', '') 
+    query = request.args.get("query", "")
 
     # use model to predict classification for query
     classification_labels = model.predict([query])[0]
     classification_results = dict(zip(df.columns[4:], classification_labels))
 
-    # This will render the go.html Please see that file. 
+    # This will render the go.html Please see that file.
     return render_template(
-        'go.html',
-        query=query,
-        classification_result=classification_results
+        "go.html", query=query, classification_result=classification_results
     )
 
 
 def main():
-    app.run(host='0.0.0.0', port=3001, debug=True)
+    app.run(host="0.0.0.0", port=3001, debug=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- data/process_data.py
+++ data/process_data.py
@@ -1,10 +1,12 @@
+# importing necessary libraries
 import sys
 import pandas as pd
 from sqlalchemy import create_engine
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# loading data from csv
 def load_data(messages_filepath, categories_filepath):
-    
+
     """
     Function that permits loading of data and merging in one df
     args :
@@ -14,17 +16,19 @@ def load_data(messages_filepath, categories_filepath):
     """
 
     # reading the csv files
-    categories = pd.read_csv("data/{}".format(categories_filepath))
-    messages = pd.read_csv("data/{}".format(messages_filepath))
+    categories = pd.read_csv("{}".format(categories_filepath))
+    messages = pd.read_csv("{}".format(messages_filepath))
 
     # merging the two csv
     df = pd.merge(messages, categories, on="id")
 
-    return df    
+    return df
 
-#------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+# cleaning the dataframe after loading from csv
 def clean_data(df):
-    
+
     """
     Function that expand categories column , clean it and merge the final df for saving
     args :
@@ -47,20 +51,21 @@ def clean_data(df):
     # dictionary as dataframe for export
     categories_ready = pd.DataFrame.from_dict(col_dict, dtype=int)
 
-    #making sur that our output is int type
+    # making sur that our output is int type
     for col in categories_ready.columns:
         categories_ready[col] = categories_ready[col].astype(int)
 
     # concatenating the message and prepared categories
-    df = pd.concat([df[["message","genre"]], categories_ready], axis=1)
-
+    df = pd.concat([df[["message", "genre"]], categories_ready], axis=1)
 
     # droping duplicates
     df = df.drop_duplicates()
 
     return df
 
-#------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+# saving data as a database after loading and cleaning
 def save_data(df, database_filepath):
     """
     Function that saves the final df to database
@@ -69,35 +74,51 @@ def save_data(df, database_filepath):
     returns:
         saved database
     """
-    engine = create_engine("sqlite:///data/{}".format(database_filepath))
+    engine = create_engine("sqlite:///{}".format(database_filepath))
     df.to_sql("messages", con=engine, if_exists="replace")
-  
-#------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
+# execution of the full process of loading, cleaning and saving
 def main():
+    """
+    Function that leverage all the other function and execute the full ETL pipeline
+    args:
+        None
+    returns:
+        Preprocessed and cleaned data in a database
+    """
+
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
 
-        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
-              .format(messages_filepath, categories_filepath))
+        print(
+            "Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}".format(
+                messages_filepath, categories_filepath
+            )
+        )
         df = load_data(messages_filepath, categories_filepath)
 
-        print('Cleaning data...')
+        print("Cleaning data...")
         df = clean_data(df)
-        
-        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
-        save_data(df, database_filepath)
-        
-        print('Cleaned data saved to database!')
-    
-    else:
-        print('Please provide the filepaths of the messages and categories '\
-              'datasets as the first and second argument respectively, as '\
-              'well as the filepath of the database to save the cleaned data '\
-              'to as the third argument. \n\nExample: python data/process_data.py '\
-              'disaster_messages.csv disaster_categories.csv '\
-              'DisasterResponse.db')
 
-#------------------------------------------------------------------------------
-if __name__ == '__main__':
+        print("Saving data...\n    DATABASE: {}".format(database_filepath))
+        save_data(df, database_filepath)
+
+        print("Cleaned data saved to database!")
+
+    else:
+        print(
+            "Please provide the filepaths of the messages and categories "
+            "datasets as the first and second argument respectively, as "
+            "well as the filepath of the database to save the cleaned data "
+            "to as the third argument. \n\nExample: python data/process_data.py "
+            "data/disaster_messages.csv data/disaster_categories.csv "
+            "data/DisasterResponse.db"
+        )
+
+
+# ------------------------------------------------------------------------------
+if __name__ == "__main__":
     main()
